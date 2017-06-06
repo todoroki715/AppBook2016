f1feb308e1c7066bbd6775365a64ee80e92af704
yasuda <ko-yasuda@trans-cosmos-tech.co.jp>
2017-06-06 15:47:20 +0900

    ログイン処理完成

--- sources/jp/co/trans/tech/servlet/MenuServlet.java
+++ sources/jp/co/trans/tech/servlet/MenuServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import jp.co.trans.tech.dto.EmployeeDto;
 import jp.co.trans.tech.formbean.ErrorFormBean;
 import jp.co.trans.tech.formbean.LoginTopFormBean;
 import jp.co.trans.tech.service.EmployeeService;
@@ -27,7 +28,6 @@ public class MenuServlet extends HttpServlet{
 			HttpSession session = request.getSession();
 
 			try{
-
 				LoginTopFormBean LoginTopForm = (LoginTopFormBean) session.getAttribute("loginTopForm");
 				String accountId = request.getParameter("accountId");
 				String pass = request.getParameter("pass");
@@ -42,6 +42,16 @@ public class MenuServlet extends HttpServlet{
 				}*/
 				EmployeeService Employee = new EmployeeService();
 				int num = Employee.doSelectCount(accountId,pass);
+				if(num == 0){
+					RequestDispatcher dispatch = request.getRequestDispatcher("./WEB-INF/jsp/loginTop.jsp");
+					dispatch.forward(request, response);
+
+				}
+				EmployeeDto Dto = Employee.doSelectPrimay(accountId);
+				LoginTopForm.setaccountId(Dto.getaccountId());
+				LoginTopForm.setaccountName(Dto.getaccountName());
+				LoginTopForm.setmasterFlg(Dto.getmasterFlg());
+				session.setAttribute("GREETING_NAME","こんにちは！" + LoginTopForm.getaccountName() + "さん");
 				RequestDispatcher dispatch = request.getRequestDispatcher("./WEB-INF/jsp/menu.jsp");
 				dispatch.forward(request, response);
 			}catch (Exception e) {
